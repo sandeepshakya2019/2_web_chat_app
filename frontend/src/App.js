ca348f1f22dda65fb1ac2f445526bf70861125e1
Sandeep Kuamr <sandeepshakya2015@gmail.com>
2024-05-06 11:20:51 +0530

    Add Pages (Login, Register, Chat and Group) => In lazy mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
-import React, { useEffect } from "react";
-import { toast } from "react-toastify";
+import React, { useEffect, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./Pages/Home";
+const Home = lazy(() => import("./Pages/Home"));
+
+const Chat = lazy(() => import("./Pages/Chat"));
+const Group = lazy(() => import("./Pages/Group"));
+
+const Login = lazy(() => import("./Pages/Login"));
+const Register = lazy(() => import("./Pages/Register"));
 
 function App() {
-  useEffect(() => {
-    toast.success("msg");
-  }, []);
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/chat/:id" element={<Chat />} />
+          <Route path="/group" element={<Group />} />
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route path="*" element={<h1>No Path Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>Home Compo s</div>;
+}
+
+export default Home;
