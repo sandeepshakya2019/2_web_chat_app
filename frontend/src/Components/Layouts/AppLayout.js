148f975909aaa560217b0702bc2a05e0b51ea53a
Sandeep Kuamr <sandeepshakya2015@gmail.com>
2024-05-09 09:34:40 +0530

    Create the Chat Component and Profile Card (CSS based)

--- frontend/src/Components/Layouts/AppLayout.js
+++ frontend/src/Components/Layouts/AppLayout.js
@@ -1,44 +1,65 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
 import Header from "./Header";
 import { Grid } from "@mui/material";
+import ChatList from "../Specific/ChatList";
+import { samepleChats } from "../../constants/sampleData";
+import { useParams } from "react-router-dom";
+import Profile from "../Specific/Profile";
 
 const AppLayout = () => (WrappedComponent) => {
-  return (props) => (
-    <div>
-      <Header />
+  return (props) => {
+    const params = useParams();
+    const chatId = params.id;
+    return (
+      <div>
+        <Header />
 
-      <Grid container height={"calc(100vh - 4rem)"}>
-        <Grid
-          item
-          sm={4}
-          md={3}
-          sx={{
-            display: { xs: "none", sm: "block" },
-          }}
-          height={"100%"}
-        >
-          All Chats
-        </Grid>
-        <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"}>
-          <WrappedComponent {...props} />
-        </Grid>
+        <Grid container height={"calc(100vh - 4rem)"}>
+          <Grid
+            item
+            sm={4}
+            md={3}
+            sx={{
+              display: { xs: "none", sm: "block" },
+            }}
+            height={"100%"}
+          >
+            <ChatList
+              w="100%"
+              chatId={String(chatId)}
+              chats={samepleChats}
+              // onlineUsers={[]}
+              // newMessagesAlert={[
+              //   {
+              //     chatId: "2",
+              //     count: 3,
+              //   },
+              // ]}
+              handleDeleteChat={() => {}}
+            />
+          </Grid>
+          <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"}>
+            <WrappedComponent {...props} />
+          </Grid>
 
-        <Grid
-          item
-          md={4}
-          lg={3}
-          height={"100%"}
-          sx={{
-            display: { xs: "none", md: "block" },
-            padding: "2rem",
-            bgcolor: "rgba(0,0,0,0.85)",
-          }}
-        >
-          User Profile
+          <Grid
+            item
+            md={4}
+            lg={3}
+            height={"100%"}
+            sx={{
+              display: { xs: "none", md: "block" },
+              padding: "2rem",
+              bgcolor: "rgba(0,0,0,0.85)",
+            }}
+          >
+            <Profile user={samepleChats[0]} />
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
-  );
+      </div>
+    );
+  };
 };
 
 export default AppLayout;
---
+++ frontend/src/Components/Shared/ChatItem.js
@@ -0,0 +1,69 @@
+import { Box, Stack, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import AvatarCard from "./AvatarCard";
+import { motion } from "framer-motion";
+
+function ChatItem({
+  avatar = [],
+  name,
+  _id,
+  groupChat = false,
+  sameSender,
+  isOnline,
+  newMessageAlert,
+  index = 0,
+  handleDeleteChat,
+}) {
+  return (
+    <Link
+      className="chat-item"
+      style={{
+        textDecoration: "none",
+      }}
+      to={`/chat/${_id}`}
+      onContextMenu={(e) => handleDeleteChat(e, _id, groupChat)}
+    >
+      <motion.div
+        initial={{ opacity: 0, y: "-100%" }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 * index }}
+        style={{
+          display: "flex",
+          gap: "1rem",
+          alignItems: "center",
+          backgroundColor: sameSender ? "rgba(0,0,0,0.3)" : "unset",
+          color: sameSender ? "black" : "black",
+          position: "relative",
+          padding: "1rem",
+        }}
+      >
+        <AvatarCard avatar={avatar} />
+
+        <Stack>
+          <Typography>{name}</Typography>
+          {newMessageAlert && (
+            <Typography>{newMessageAlert.count} New Message</Typography>
+          )}
+        </Stack>
+
+        {isOnline && (
+          <Box
+            sx={{
+              width: "10px",
+              height: "10px",
+              borderRadius: "50%",
+              backgroundColor: "green",
+              position: "absolute",
+              top: "50%",
+              right: "1rem",
+              transform: "translateY(-50%)",
+            }}
+          />
+        )}
+      </motion.div>
+    </Link>
+  );
+}
+
+export default ChatItem;
